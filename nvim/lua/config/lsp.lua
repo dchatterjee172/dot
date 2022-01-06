ffbdac2acd7d1f0de89fad65f4f5b06573182673
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2022-01-07 00:28:08 +0530

    add pylint

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,6 +1,5 @@
 local lspconfig = require("lspconfig")
 local coq = require("coq")
-
 lspconfig.jedi_language_server.setup(coq.lsp_ensure_capabilities({ documentFormatting = false }))
 lspconfig.efm.setup({
 	init_options = { documentFormatting = true },
@@ -9,6 +8,20 @@ lspconfig.efm.setup({
 		languages = {
 			python = {
 				{ formatCommand = "black --quiet -", formatStdin = true },
+				{
+					lintCommand = "pylint --output-format text --score no --msg-template {path}:{line}:{column}:{C}:{msg} ${INPUT}",
+					lintFormats = { "%f:%l:%c:%t:%m" },
+					lintOffsetColumns = 1,
+					lintStdin = false,
+					lintCategoryMap = {
+						I = "H",
+						R = "I",
+						C = "I",
+						W = "W",
+						E = "E",
+						F = "E",
+					},
+				},
 			},
 		},
 	},
