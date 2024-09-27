cee53f6ec15e41980030f9039621f5e2ad5b88ec
dchatterjee172 <dchatterjee172@gmail.com>
2024-09-27 08:37:26 -0700

    ruff lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -2,32 +2,39 @@ local lspconfig = require("lspconfig")
 local lspconfig_util = require("lspconfig/util")
 local coq = require("coq")
 lspconfig.jedi_language_server.setup(coq.lsp_ensure_capabilities({ documentFormatting = false }))
-lspconfig.efm.setup({
-	init_options = { documentFormatting = true },
-	filetypes = { "python" },
-	settings = {
-		rootMarkers = { ".git" } ,
-		languages = {
-			python = {
-				{ formatCommand = "black --quiet -", formatStdin = true },
-				{
-					lintCommand = "pylint --output-format text --score no --msg-template {path}:{line}:{column}:{C}:{msg} ${INPUT}",
-					lintFormats = { "%f:%l:%c:%t:%m" },
-					lintOffsetColumns = 1,
-					lintStdin = false,
-					lintCategoryMap = {
-						I = "H",
-						R = "I",
-						C = "I",
-						W = "W",
-						E = "E",
-						F = "E",
-					},
-				},
-			},
-		},
-	},
-})
+require('lspconfig').ruff_lsp.setup {
+  init_options = {
+    settings = {
+      args = {},
+    }
+  }
+}
+-- lspconfig.efm.setup({
+-- 	init_options = { documentFormatting = true },
+-- 	filetypes = { "python" },
+-- 	settings = {
+-- 		rootMarkers = { ".git" } ,
+-- 		languages = {
+-- 			python = {
+-- 				{ formatCommand = "black --quiet -", formatStdin = true },
+-- 				{
+-- 					lintCommand = "pylint --output-format text --score no --msg-template {path}:{line}:{column}:{C}:{msg} ${INPUT}",
+-- 					lintFormats = { "%f:%l:%c:%t:%m" },
+-- 					lintOffsetColumns = 1,
+-- 					lintStdin = false,
+-- 					lintCategoryMap = {
+-- 						I = "H",
+-- 						R = "I",
+-- 						C = "I",
+-- 						W = "W",
+-- 						E = "E",
+-- 						F = "E",
+-- 					},
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- })
 lspconfig.gopls.setup(coq.lsp_ensure_capabilities({
 	cmd = { "gopls", "serve" },
 	filetypes = { "go", "gomod" },
